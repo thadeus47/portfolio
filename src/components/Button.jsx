03f10d75135600730c0b20e12a2a79b3c3eb7088
m_thado <tmiseda@gmail.com>
2024-11-16 17:34:54 -0500

    hero section updated

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
-
-const Button = ({ name, isBeam = false, containerClass}) => {
-  return (
-    <button className={`btn ${containerClass}`}>
+const Button = ({ name, isBeam = false, containerClass }) => {
+    return (
+      <button className={`btn ${containerClass}`}>
         {isBeam && (
-            <span className="relative flex h-3 w-3">
-                <span className="btn-ping" />
-                <span className="btn-ping_dot" />
-            </span>
+          <span className="relative flex h-3 w-3">
+            <span className="btn-ping"></span>
+            <span className="btn-ping_dot"></span>
+          </span>
         )}
         {name}
-    </button>
-  )
-}
-
-export default Button
+      </button>
+    );
+  };
+  
+  export default Button;
--- src/components/HeroCamera.jsx
+++ src/components/HeroCamera.jsx
@@ -1,23 +1,19 @@
-import { useFrame } from "@react-three/fiber";
-import { easing } from "maath";
-import { useRef } from "react"
+import { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { easing } from 'maath';
 
+const HeroCamera = ({ isMobile, children }) => {
+  const group = useRef();
 
-const HeroCamera = ({ children, isMobile }) => {
-    const groupRef = useRef();
+  useFrame((state, delta) => {
+    easing.damp3(state.camera.position, [0, 0, 20], 0.25, delta);
 
-    useFrame ((state, delta) => {
-      easing.damp3(state.camera.position, [0, 0, 20], 0.25, delta);
-    })
-
-    if(!isMobile) {
-      useFrame ((state, delta) => {
-        easing.dampE(groupRef.current.rotation,[-state.pointer.y / 3, -state.pointer.x / 5, 0] );
-      })
+    if (!isMobile) {
+      easing.dampE(group.current.rotation, [-state.pointer.y / 3, state.pointer.x / 5, 0], 0.25, delta);
     }
-  return (
-    <group ref={groupRef} scale={isMobile ? 1 : 1.3}>{children}</group>
-  )
-}
+  });
 
-export default HeroCamera
+  return <group ref={group}>{children}</group>;
+};
+
+export default HeroCamera;
