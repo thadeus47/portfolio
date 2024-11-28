63e538680440201d506ebf727ed95babba1e259a
m_thado <tmiseda@gmail.com>
2024-11-28 11:19:17 -0500

    updates

---
+++ src/sections/About.jsx
@@ -0,0 +1,17 @@
+
+
+const About = () => {
+  return (
+    <section className="c-space my-20">
+        <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
+            <div className="col-span-1 xl:row-span-3">
+                <div className="grid-container">
+                    <img src="./assets/grid-1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default About
